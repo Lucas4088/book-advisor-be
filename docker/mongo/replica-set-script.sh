9c49d3d6602d926185f32fe619d14127df274f1b
lukasz <lukasz4088@gmail.com>
2026-03-19 14:06:20 +0000

    adjustments to remote docker-compoose pt 2

--- docker/mongo/replica-set-script.sh
+++ docker/mongo/replica-set-script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -e
+
 mkdir -p /etc/mongo
 cp /tmp/keyfile /etc/mongo/keyfile
-chmod 400 /etc/mongo/keyfile
+
+chmod 600 /etc/mongo/keyfile
 chown mongodb:mongodb /etc/mongo/keyfile
-exec docker-entrypoint.sh mongod --replSet rs0 --keyFile /etc/mongo/keyfile --bind_ip_all
+
+exec docker-entrypoint.sh mongod \
+  --replSet rs0 \
+  --keyFile /etc/mongo/keyfile \
+  --bind_ip_all
